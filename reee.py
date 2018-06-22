b498e465b93d5059ae0f9fbbb11d6ea781024db2
varma9803 <akhilvarma1998@gmail.com>
2018-06-22 14:29:59 +0530

    my_first_command

---
+++ reee.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+import os
+import sqlite3 as sql
+con=sql.connect("C:Users/AKHILVARMA/Desktop/rd")
+
+cur=con.cursor()
+
+cur.execute
+con_sho=sql.connect("shopping.db")
+cur_sho=con_pro.cursor();
+cur_sho.execute("create table if not exists pro url LONGVARCHAR")
